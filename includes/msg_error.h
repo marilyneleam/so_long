83cb94666af04666e62d10d1f292916a1f2f3e82
mleam <mleam@student.42.fr>
2022-02-25 19:15:05 +0100

    add fuctions

--- includes/msg_error.h
+++ includes/msg_error.h
@@ -6,7 +6,7 @@
 /*   By: mleam <mleam@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 15:25:47 by mleam             #+#    #+#             */
-/*   Updated: 2022/02/25 15:55:11 by mleam            ###   ########.fr       */
+/*   Updated: 2022/02/25 18:25:28 by mleam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,11 @@
 # define ERR_DIRECTORY "File is a directory\n"
 # define ERR_NAME "File can't be named .ber\n"
 # define ERR_MAP_SMALL "Map is too small\n"
+# define ERR_MAP_RECTANGLE "Map is not rectangle\n"
+# define ERR_MAP_CLOSED "Map is not closed\n"
+# define ERR_PARAM_PLAYER "Miss or too much players in the game\n"
+# define ERR_PARAM_COLL "Miss collectibles in the game\n"
+# define ERR_PARAM_EXIT "Miss exit in the game\n"
+# define ERR_PARAM "Parameter does not exist\n"
 
 #endif
--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: mleam <mleam@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 23:28:14 by mleam             #+#    #+#             */
-/*   Updated: 2022/02/25 16:29:16 by mleam            ###   ########.fr       */
+/*   Updated: 2022/02/25 19:07:40 by mleam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,34 @@
 # include <sys/stat.h>
 # include <fcntl.h>
 
+# define PLAYER "textures/player.xpm"
+# define COLLECTIBLES "textures/ball.xpm"
+# define EXIT "textures/exit.xpm"
+# define WALL "tree.xpm"
+# define GROUND "textures/sol.xpm"
+
 typedef struct s_map
 {
 	int		row;
 	int		column;
 	char	**tab;
+	int		exit;
+	int		coll;
+	int		player;
 }				t_map;
 
 void	check_extension(char *str);
 void	check_and_read_file(char **av);
-void	check_validity_map(char **av, t_map *map);
+void	check_map_size(char **av, t_map *map);
 void	copy_map(char **av, t_map *map);
+void	check_map_rectangle(t_map *map);
+void	check_map_wall(t_map *map);
+void	count_map_param(t_map *map);
+void	check_map_param(t_map *map);
+void	print_error(char *s);
+void	check_texture_extension(char *str);
+void	check_and_read_texture(char *pathfile);
+void	check_texture();
 int		main(int ac, char **av);
 
 #endif
